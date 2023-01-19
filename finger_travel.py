e5760e03a0267b6c88e54ecaa2573c6c976c99ad
Youssef Akoudad <akoudadyf@gmail.com>
2023-01-19 19:44:11 +0100

    Create finger_travel.py

---
+++ finger_travel.py
@@ -0,0 +1,17 @@
+qwerty_keyboard = {
+    'q': (0, 0), 'w': (0, 1), 'e': (0, 2), 'r': (0, 3), 't': (0, 4), 'y': (0, 5), 'u': (0, 6), 'i': (0, 7), 'o': (0, 8), 'p': (0, 9),
+    'a': (1, 0), 's': (1, 1), 'd': (1, 2), 'f': (1, 3), 'g': (1, 4), 'h': (1, 5), 'j': (1, 6), 'k': (1, 7), 'l': (1, 8),
+    'z': (2, 0), 'x': (2, 1), 'c': (2, 2), 'v': (2, 3), 'b': (2, 4), 'n': (2, 5), 'm': (2, 6)
+}
+
+from math import sqrt
+
+def finger_travel(word: str) -> float:
+    x1, y1 = qwerty_keyboard[word[0].lower()]
+    total_distance = 0
+    for letter in word[1:]:
+        x2, y2 = qwerty_keyboard[letter.lower()]
+        distance = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        total_distance += distance
+        x1, y1 = x2, y2
+    return total_distance
